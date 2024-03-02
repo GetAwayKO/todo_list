a7c376f7770abc56174fdae292e747ba9d7047c4
Vladislav Komarowsckikh <komarowsckikh@yandex.ru>
2024-03-03 00:51:11 +0300

    added homepage

--- client/views/Homepage/Home.jsx
+++ client/views/Homepage/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-
+import styles from "./Home.module.scss";
+import LinkButton from "../../components/button/LinkButton";
 const Home = () => {
-  return <div>This is Homepage</div>;
+  return (
+    <div className={styles.home}>
+      <h3>To Do list for your organizing</h3>
+      <div className={styles.container}>
+        <LinkButton to="/list">Get started</LinkButton>
+      </div>
+    </div>
+  );
 };
 export default Home;
