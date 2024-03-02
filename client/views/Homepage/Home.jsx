6e85409e08b4500ab4f5b890a22d42f693c8b1da
Vladislav Komarowsckikh <komarowsckikh@yandex.ru>
2024-03-03 01:44:54 +0300

    finished homepage

--- client/views/Homepage/Home.jsx
+++ client/views/Homepage/Home.jsx
@@ -4,7 +4,10 @@ import LinkButton from "../../components/button/LinkButton";
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h3>To Do list for your organizing</h3>
+      <div className={styles.titles}>
+        <h2>To-do list for your organizing</h2>
+        <h3>It's just a to-do list and nothing more</h3>
+      </div>
       <div className={styles.container}>
         <LinkButton to="/list">Get started</LinkButton>
       </div>
