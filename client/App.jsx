bcecfc564edf42a3d1b21b675ea540e4640ca4ef
Vladislav Komarowsckikh <komarowsckikh@yandex.ru>
2024-03-03 21:03:46 +0300

    todolistedit was removed

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/Homepage/Home";
 import ToDoList from "./views/ToDoList/ToDoList";
-import ToDoListEdit from "./views/ToDoList/ToDoListEdit";
 import AppLayout from "./components/layout/AppLayout";
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<ToDoList />} />
-        <Route path="/edit" element={<ToDoListEdit />} />
       </Routes>
     </AppLayout>
   );
