3a9e470dc5d0a03e76cd3b4f58ec8bc70e08b7fc
Vladislav Komarowsckikh <komarowsckikh@yandex.ru>
2024-03-04 09:53:51 +0300

    added botstrap icons

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.scss";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import "bootstrap-icons/font/bootstrap-icons.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
