02e76401f07bc12794408bcabef6403934a29c3b
Vladislav Komarowsckikh <komarowsckikh@yandex.ru>
2024-03-04 16:01:48 +0300

    added selector for filtering list

--- client/views/ToDoList/ToDoList.jsx
+++ client/views/ToDoList/ToDoList.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ToDoList.module.scss";
 import TaskList from "./Task";
+import Selector from "../../components/selector/Selector";
 const ToDoList = () => {
+  const FILTER = {
+    all: {
+      name: "All",
+      filter: () => {
+        return true;
+      },
+    },
+    uncompleted: {
+      name: "Uncompleted",
+      filter: (item) => {
+        return item.status === false;
+      },
+    },
+    completed: {
+      name: "Completed",
+      filter: (item) => {
+        return item.status === true;
+      },
+    },
+  };
+  const OPTIONS = [FILTER.all, FILTER.uncompleted, FILTER.completed];
+
+  const [filterOption, setFilterOption] = useState(FILTER.all);
   return (
     <div className={styles.wrapper}>
-      <TaskList />
+      <h2 className={styles.header}>TO DO LIST</h2>
+      <div className={styles.containerSelect}>
+        <Selector
+          onSelect={(e) => setFilterOption(e.target.value)}
+          options={OPTIONS}
+        />
+      </div>
+      <TaskList filter={filterOption.filter} />
     </div>
   );
 };
